917726425ed715f006c400ade7ae045995fd0745
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-22 11:19:10 +0300

    added new functions

--- routes/person.js
+++ routes/person.js
@@ -1,30 +1,31 @@
 var express = require("express");
 var router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 var users = [
     {
-        id: "s68d434z364",
+        id: uuidv4(),
         name: "Jason",
         surname: "Test",
         age: 20,
         sex: "Male"
     },
     {
-        id: "jlskf65a6asdf",
+        id: uuidv4(),
         name: "Emenike",
         surname: "Test",
         age: 40,
         sex: "Male"
     },
     {
-        id: "şsmd65ad5s4",
+        id: uuidv4(),
         name: "Jessica",
         surname: "Test",
         age: 21,
         sex: "Famale"
     },
     {
-        id: "lkjs65a563xcv",
+        id: uuidv4(),
         name: "Jesus",
         surname: "Test",
         age: 26,
@@ -32,26 +33,63 @@ var users = [
     },
 ]
 
-const messages = [
-    {
-        nullMessage: "there are null params"
-    }
-]
+const messages = {
+    nullMessage: "there are null params",
+    enterId:"enter an id"
+}
 
-router.get("/", function (req, res, next) {
+
+router.get("/getpersons", function (req, res, next) {
     if (req.body.id == undefined)
         res.json(users);
     else
         res.json(users.filter(x => x.id == req.body.id));
 })
 
-router.post("/", function (req, res, next) {
+router.post("/addperson", function (req, res, next) {
     if (req.body.name == undefined || req.body.surname == undefined)
-        req.json(messages);
+        res.json(messages.nullMessage);
     else {
-        req.json("kaydetmeye hazır");
+        var user = req.body;
+        user.id = uuidv4();
+
+        console.log(user);
+
+        users.push(user);
+        res.json(user);
     }
 
+})
+
+router.put("/updateperson", function (req, res, next) {
+
+
+
+    if (req.body.id == undefined)
+        res.json(messages.enterId)
+    else {
+        let body_id = req.body.id;
+
+        let keys = Object.keys(req.body)
+
+        keys.forEach(x => {
+            users.find(x => x.id == body_id)[x] = req.body[x]
+        })
+
+        res.json(users.find(x => x.id == body_id));
+    }
+
+})
+
+router.delete("/deleteperson", function (req, res, next) {
+    if (req.body.id == undefined)
+        res.json(messages.enterId);
+
+    else {
+        let body_id = req.body.id;
+
+
+    }
 })
 
 
