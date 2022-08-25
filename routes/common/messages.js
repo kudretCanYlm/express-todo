bc9a009f83514fed99c322defdb2eba1b33bed6f
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-25 17:20:42 +0300

    first commit

---
+++ routes/common/messages.js
@@ -0,0 +1,9 @@
+const NULL_MESSAGE = "there are null params";
+const ENTER_ID_MESSAGE = "enter an id";
+const NOT_FOUND_MESSAGE = "there is not content";
+
+module.exports = {
+    NULL_MESSAGE,
+    ENTER_ID_MESSAGE,
+    NOT_FOUND_MESSAGE
+};
