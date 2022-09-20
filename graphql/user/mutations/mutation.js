189afd30ea97fc079017e7ff6682258a2547cff8
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-20 20:57:31 +0300

    fixed AddUser mutation and added AddTodoToUser,DeleteUser mutations

--- graphql/user/mutations/mutation.js
+++ graphql/user/mutations/mutation.js
@@ -1,10 +1,9 @@
-const { TodoType } = require('../../todo/type/type');
-var GraphQLList = require('graphql').GraphQLList;
-var UserModel = require("../../../utils/db/post-provier").Users;
+var UserModel = require("../../../utils/db/post-provider").Users;
+var TodoModel = require("../../../utils/db/post-provider").Todos;
 var UserType = require("../type/type").UserType;
-var GraphQLNonNull=require("graphql").GraphQLNonNull;
-var GraphQLString=require("graphql").GraphQLString;
-var GraphQLInt=require("graphql").GraphQLInt;
+var GraphQLNonNull = require("graphql").GraphQLNonNull;
+var GraphQLString = require("graphql").GraphQLString;
+var GraphQLInt = require("graphql").GraphQLInt;
 
 const AddUser = {
     type: UserType,
@@ -28,11 +27,65 @@ const AddUser = {
         const userModel = new UserModel(params);
         const newUser = userModel.save();
 
-        if (!newTask) {
+        if (!newUser) {
             throw new Error('Error');
         }
-        return newTask
+        return newUser
     }
 }
 
-module.exports = { AddUser }
+const AddTodoToUser = {
+    type: UserType,
+    args: {
+        userId: {
+            type: new GraphQLNonNull(GraphQLString)
+        },
+        todoId: {
+            type: new GraphQLNonNull(GraphQLString)
+        }
+    },
+    resolve(root, { userId, todoId }) {
+
+        TodoModel.findOne({ _id: todoId }, function (err, todo) {
+
+            UserModel.findOneAndUpdate(
+                {
+                    _id: userId
+                },
+                {
+                    $push:
+                    {
+                        todo: todo
+                    }
+                }).exec();
+
+
+        });
+
+        return UserModel.findById(userId).exec();
+
+    }
+}
+
+const DeleteUser = {
+    type: UserType,
+    args: {
+        userId: {
+            type: new GraphQLNonNull(GraphQLString)
+        }
+    },
+    resolve(root, { userId }) {
+
+        if (!userId)
+            throw new Error("userId is null");
+
+        const user = UserModel.findByIdAndDelete(userId).exec();
+
+        if (!user)
+            throw new Error("there isn't any user");
+
+        return user;
+    }
+}
+
+module.exports = { AddUser, AddTodoToUser, DeleteUser }
